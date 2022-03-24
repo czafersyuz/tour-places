1d36b1cc4f1f0da2a93a664c332cf5fbc6c87583
czafersyuz <czafersyuz@gmail.com>
2022-03-24 15:59:14 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Cards from './components/cards/Cards.js';
+import Header from './components/header/Header';
+import Navbar from './components/navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Header/>
+      <Cards/>
+      
+
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+
+import "./Header.css";
+
+function Header() {
+  return (
+    <div>
+        <h3 className="big-title">
+            POPULAR TOUR PLACES
+        </h3>
+    </div>
+  )
+}
+
+export default Header
